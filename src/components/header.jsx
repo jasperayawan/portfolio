b2fdd0f3b87e74dbadc0d9f32528327790acb7c3
jasperayawan <ejayawan22@gmail.com>
2023-05-27 18:07:10 +0800

    update head

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,11 @@
  export default function Header(){
     return(
         <>
-            <header className='relative w-full'>
-                <nav className='pointer-events-auto fixed z-50 w-full flex justify-center'>
-                            <ul className='flex items-center text-sm w-full justify-center px-5 py-3 ring-1 ring-zinc-900/5 dark:text-zinc-200 backdrop-blur gap-5 shadow-lg text-zinc-800 font-medium'>
+            <header className='relative'>
+                <div className='mx-auto max-w-2xl lg:max-w-5xl'>
+                    <div className='flex flex-1 justify-center'>
+                        <nav className='py-4 pointer-events-auto hidden md:block fixed z-50'>
+                            <ul className='flex rounded-full bg-white/90 text-sm px-5 py-3 ring-1 ring-zinc-900/5 dark:text-zinc-200 backdrop-blur gap-5 shadow-lg text-zinc-800 font-medium'>
                                 <li>
                                     <Link>About</Link>
                                 </li>
@@ -19,7 +21,9 @@
                                     <Link>Contact</Link>
                                 </li>
                             </ul>
-                </nav>
+                        </nav>
+                    </div>
+                </div>
             </header>
         </>
     )
