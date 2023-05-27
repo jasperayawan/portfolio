5681431f086e3a5f9253eb2293c0b7984e6b0262
jasperayawan <ejayawan22@gmail.com>
2023-05-27 15:07:10 +0800

    update header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@
             <header className='relative'>
                 <div className='mx-auto max-w-2xl lg:max-w-5xl'>
                     <div className='flex flex-1 justify-center'>
-                        <nav className='py-4 pointer-events-auto hidden md:block'>
+                        <nav className='py-4 pointer-events-auto fixed z-50'>
                             <ul className='flex rounded-full bg-white/90 text-sm px-5 py-3 ring-1 ring-zinc-900/5 dark:text-zinc-200 backdrop-blur gap-5 shadow-lg text-zinc-800 font-medium'>
                                 <li>
                                     <Link>About</Link>
@@ -15,7 +15,7 @@
                                     <Link>Articles</Link>
                                 </li>
                                 <li>
-                                    <Link>Projects</Link>
+                                    <Link to='/project'>Projects</Link>
                                 </li>
                                 <li>
                                     <Link>Contact</Link>
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom'
 import InstaProject from '../assets/insta.png'
 import { AiFillGithub } from 'react-icons/ai'
 import Harvesthub from '../assets/harvesthub.png'
+import ELTech from '../assets/eltech.png'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Projects(){
     return(
         <>
-            <div className="my-[10rem] mx-auto max-w-2xl lg:max-w-5xl flex flex-col items-center justify-center gap-28 pt-10">
+            <Header/>
+            <div className="my-[5rem] mx-auto max-w-2xl lg:max-w-5xl flex flex-col items-center justify-center gap-28 pt-10">
                 <div className='me-auto'>
                     <h1 className='text-2xl font-bold text-zinc-800'>My Project</h1>
                 </div>
@@ -39,7 +43,9 @@ export default function Projects(){
                             </li>
                         </ul>
                         <div className='flex gap-5'> 
-                            <AiFillGithub className='text-2xl text-zinc-600 dark:text-zinc-200'/>
+                            <Link to='https://github.com/jasperayawan/Instagram-clone'>
+                                <AiFillGithub className='text-2xl text-zinc-600 dark:text-zinc-200'/>
+                            </Link>
                         </div>
                     </div>
                 </div>  
@@ -73,12 +79,51 @@ export default function Projects(){
                             </li>
                         </ul>
                         <div className='flex gap-5'> 
-                            <AiFillGithub className='text-2xl text-zinc-600 dark:text-zinc-200'/>
+                            <Link to='https://github.com/jasperayawan/miniP2'>
+                                <AiFillGithub className='text-2xl text-zinc-600 dark:text-zinc-200'/>
+                            </Link>
                         </div>
                     </div>
                 </div> 
 
+                <div className="flex flex-col xl:flex-row gap-6">
+                    <Link className='w-full xl:w-1/2 h-auto relative group' to='https://jasperayawan.github.io/eLTECH/'>
+                        <div className=''>
+                            <img src={ELTech} alt="" className='ring-1 rounded-md ring-zinc-200'/>
+                        </div>
+                    </Link>
+                    <div className='w-full xl:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right xl:-ml-16 z-10'>
+                        <div>
+                            <p className='font-bold text-zinc-800 dark:text-zinc-400'>Featured Project</p>
+                            <h2 className='font-bold text-2xl text-slate-600'>E-Learning Technology</h2>
+                        </div>
+                        <p className='bg-slate-800 xl:-mr-16 text-slate-400 md:text-base p-2 md:p-6 rounded-md'>
+                            An Instagram clone for visualizing Instagram website. using reactjs and tailwindcss
+                        </p>
+                        <ul className='text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between'>
+                            <li>
+                                <span className='text-zinc-500'>HTML</span>
+                            </li>
+                            <li>
+                                <span className='text-zinc-500'>CSS</span>
+                            </li>
+                            <li>
+                                <span className='text-zinc-500'>Javascript</span>
+                            </li>
+                            <li>
+                                <span className='text-zinc-500'>Github Deployment</span>
+                            </li>
+                        </ul>
+                        <div className='flex gap-5'> 
+                            <Link to='https://github.com/jasperayawan/eLTECH'>
+                                <AiFillGithub className='text-2xl text-zinc-600 dark:text-zinc-200'/>
+                            </Link>
+                        </div>
+                    </div>
+                </div> 
             </div>
+
+            <Footer/>
         </>
     )
 }
